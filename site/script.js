84d656ba9cce7b91ed8f38756094c0ae05aba1fb
Cameron McConnell <cameron@angry-dogman.com>
2024-07-29 09:30:18 -0500

    Added skills, contact, and about sections.

--- site/script.js
+++ site/script.js
@@ -56,15 +56,18 @@ class CommandParser {
     #executeHelp() {
         const helpHTML =
         `<div class="margin-bottom flex-column flex-row-gap">
+            <p> Welcome to My Developer Portfolio CLI! 👋</p>
+            <p>This interface allows you to navigate and explore my projects and experience using command-line commands.</p>
             <p>Valid Commands:</p>
             <ul>
-                <li><strong>help</strong>: Display this help message.</li>
-                <li><strong>projects</strong>: Show a list of my projects.</li>
-                <li><strong>about</strong>: Learn more about me.</li>
-                <li><strong>skills</strong>: View my skill set.</li>
-                <li><strong>contact</strong>: Find out how to get in touch with me.</li>
-                <li><strong>clear</strong>: Remove all previous output.</li>
+                <li><strong>help:</strong> Display this help message.</li>
+                <li><strong>projects:</strong> Show a list of my projects.</li>
+                <li><strong>about:</strong> Learn more about me.</li>
+                <li><strong>skills:</strong> View my skill set.</li>
+                <li><strong>contact:</strong> Find out how to get in touch with me.</li>
+                <li><strong>clear:</strong> Remove all previous output.</li>
             </ul>
+            <p>Type a command to get started.</p>
         </div>`
 
         this.#mainContainer.insertAdjacentHTML('beforeend', helpHTML)
@@ -77,14 +80,54 @@ class CommandParser {
     }
 
     #executeAbout() {
+        const aboutHTML = 
+        `<div class="margin-bottom flex-column flex-row-gap">
+            <p><strong>About Me 👨‍💻</strong></p>
+            <p>
+                I am a recent Computer Science graduate from Arizona State University 
+                with a deep passion for technology and development. My journey has been 
+                shaped by diverse experiences in collaborative environments where I've worked 
+                on innovative projects. My background includes roles as a lead tutor and teaching 
+                assistant, where I honed my skills in leadership and communication. I'm enthusiastic about 
+                leveraging my knowledge and experience to contribute to forward-thinking tech companies 
+                and exciting new challenges.
+            </p>
+        </div>`
+
+        this.#mainContainer.insertAdjacentHTML('beforeend', aboutHTML)
+
         this.#appendCliToMainContainer()
     }
 
     #executeSkills() {
+        const skillsHTML =
+        `<div class="margin-bottom flex-column flex-row-gap">
+            <p><strong>Skills 🥷</strong></p>
+            <ul>
+                <li><strong>Programming Languages:</strong> Java, JavaScript/TypeScript, C/C++, Python, Go, Rust</li>
+                <li><strong>Web Development:</strong> HTML, CSS, SCSS, Tailwind CSS, React.js, React Native, Svelte</li>
+                <li><strong>Tools & Technologies:</strong> npm, Pip, Cargo, MySQL, MongoDB, GraphQL, Git, Docker, Debian, Ubuntu, AWS, Agile</li>
+            </ul>
+        </div>`
+
+        this.#mainContainer.insertAdjacentHTML('beforeend', skillsHTML)
+
         this.#appendCliToMainContainer()
     }
 
     #executeContact() {
+        const contactHTML =
+        `<div class="margin-bottom flex-column flex-row-gap">
+            <p><strong>Contact Information 📨</strong></p>
+            <ul>
+                <li><strong>Email: </strong><a href="mailto:cameron.mcconne@gmail.com" target="_blank">cameron.mcconne@gmail.com</a></li>
+                <li><strong>LinkedIn: </strong><a href="https://linkedin.com/in/cameron-mcconnell-704b17225" target="_blank">linkedin.com/in/cameron-mcconnell</a></li>
+                <li><strong>GitHub: </strong><a href="https://github.com/cameronMcConnell" target="_blank">github.com/cameronMcConnell</a></li>
+            </ul>
+        </div>`
+
+        this.#mainContainer.insertAdjacentHTML('beforeend', contactHTML)
+
         this.#appendCliToMainContainer()
     }
 

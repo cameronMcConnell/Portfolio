29159406de4101f95040f46d06172215eb2c4b76
Cameron McConnell <cameron@angry-dogman.com>
2024-07-28 23:45:20 -0500

    Added scroll to input element, basic functionality for each command, and clear command.
    Had to move some of the counter execution to the CommandParser class. This allows for the
    parser to better implement a command like clear.

--- site/script.js
+++ site/script.js
@@ -1,20 +1,30 @@
 class CLICounter {
+    #count
+
     constructor() {
-        this.count = 0
+        this.#count = 0
     }
 
     incrementCount() {
-        this.count++
+        this.#count++
+    }
+
+    resetCount() {
+        this.#count = 0
     }
 
     getCount() {
-        return this.count
+        return this.#count
     }
 }
 
 class CommandParser {
-    constructor() {
-        this.mainContainer = document.getElementById("main-container")
+    #counter
+    #mainContainer
+    
+    constructor(counter) {
+        this.#counter = counter
+        this.#mainContainer = document.getElementById("main-container")
     }
 
     parseCommand(command) {
@@ -34,6 +44,9 @@ class CommandParser {
             case 'contact':
                 this.#executeContact()
                 break
+            case 'clear':
+                this.#executeClear()
+                break
             default:
                 this.#executeDefault()
                 break
@@ -43,8 +56,6 @@ class CommandParser {
     #executeHelp() {
         const helpHTML =
         `<div class="margin-bottom flex-column flex-row-gap">
-            <p> Welcome to My Developer Portfolio CLI! </p>
-            <p>This interface allows you to navigate and explore my projects and experience using command-line commands.</p>
             <p>Valid Commands:</p>
             <ul>
                 <li><strong>help</strong>: Display this help message.</li>
@@ -52,60 +63,81 @@ class CommandParser {
                 <li><strong>about</strong>: Learn more about me.</li>
                 <li><strong>skills</strong>: View my skill set.</li>
                 <li><strong>contact</strong>: Find out how to get in touch with me.</li>
+                <li><strong>clear</strong>: Remove all previous output.</li>
             </ul>
-            <p>Type a command to get started.</p>
         </div>`
 
-        this.mainContainer.insertAdjacentHTML('beforeend', helpHTML)
+        this.#mainContainer.insertAdjacentHTML('beforeend', helpHTML)
 
+        this.#appendCliToMainContainer()
+    }
+
+    #executeProjects() {
+        this.#appendCliToMainContainer()
+    }
+
+    #executeAbout() {
+        this.#appendCliToMainContainer()
+    }
+
+    #executeSkills() {
+        this.#appendCliToMainContainer()
+    }
+
+    #executeContact() {
+        this.#appendCliToMainContainer()
+    }
+
+    #executeClear() {
+        this.#counter.resetCount()
+
+        this.#mainContainer.innerHTML = 
+        `<div class="margin-bottom flex-row flex-col-gap">
+            <p><strong>root:~</strong>$</p>
+            <input class="command-line" type="text" maxlength="8">
+        </div>`
+    }
+
+    #executeDefault() {
+        const defaultHTML =
+        `<div class="margin-bottom">
+            <p>Invalid Command (╯°□°)╯</p>
+        </div>`
+
+        this.#mainContainer.insertAdjacentHTML('beforeend', defaultHTML)
+
+        this.#appendCliToMainContainer()
+    }
+
+    #appendCliToMainContainer() {
         const cliHTML = 
         `<div class="margin-bottom flex-row flex-col-gap">
             <p><strong>root:~</strong>$</p>
             <input class="command-line" type="text" maxlength="8">
         </div>`
 
-        this.mainContainer.insertAdjacentHTML('beforeend', cliHTML)
-    }
+        this.#mainContainer.insertAdjacentHTML('beforeend', cliHTML)
 
-    #executeProjects() {
-
-    }
-
-    #executeAbout() {
-
-    }
-
-    #executeSkills() {
-
-    }
-
-    #executeContact() {
-
-    }
-
-    #executeDefault() {
-
+        this.#counter.incrementCount()
     }
 }
 
 window.addEventListener('load', () => {
-
-    const parser = new CommandParser()
-
     const counter = new CLICounter()
+
+    const parser = new CommandParser(counter)
 
     let currentCli = document.getElementsByClassName('command-line')[0]
     
-    currentCli.focus()
     currentCli.select()
 
     const handleEnterKeyOnInput = (e) => {
         if (e.key === 'Enter') {
             parser.parseCommand(currentCli.value)
-            counter.incrementCount()
             currentCli.disabled = true
             currentCli = document.getElementsByClassName('command-line')[counter.getCount()]
             currentCli.select()
+            window.scrollTo(currentCli.offsetLeft, currentCli.offsetTop)
             currentCli.addEventListener('keyup', (e) => handleEnterKeyOnInput(e))
         }
     }

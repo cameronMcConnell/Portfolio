e4fab14d7f94c20816ca2051e64745c3c128ab66
Cameron McConnell <cameron@angry-dogman.com>
2024-10-22 13:46:45 -0700

    Init snake.js file and added it to index.html

---
+++ site/snake.js
@@ -0,0 +1,11 @@
+class SnakeGame {
+    constructor() {
+        console.log('new snake game');
+    }
+
+    startGame() {
+        console.log('starting game');
+    }
+}
+
+window.SnakeGame = SnakeGame;
